bb04e84c330340321ed3233e4e070f6817e6ae0c
imed jaberi <imed_jebari@hotmail.fr>
2019-08-23 17:54:21 +0200

    update test method : using ` mocha ☕️ ` & ` chai 🍵 ` 👻 ..

--- test/package-test.js
+++ test/package-test.js
@@ -1,4 +1,4 @@
-const assert = require('assert');
+const { expect } = require('chai');
 const package = require('../src/main') ; 
 
 
@@ -8,93 +8,93 @@ describe(
 
      it ('romain to romain', () => {
           let result = package ("MCDLVI","romain") ; 
-          assert.equal(result,"MCDLVI");
+          expect(result).to.equal("MCDLVI");
      });
 
      it ('romain to modern', () => {
           let result = package ("MCDLVI","modern") ; 
-          assert.equal(result,"1456");
+          expect(result.toString()).to.equal("1456");
      });
 
      it ('romain to arabic', () => {
           let result = package ("MCDLVI","arabic") ; 
-          assert.equal(result,"۱٤٥٦");
+          expect(result).to.equal("۱٤٥٦");
      }); 
 
      it ('romain to persian', () => {
           let result = package ("MCDLVI","persian") ; 
-          assert.equal(result,"۱۴۵۶");
+          expect(result).to.equal("۱۴۵۶");
      });     
 
      it ('modern to modern', () => {
           let result = package ("1456","modern") ; 
-          assert.equal(result,"1456");
+          expect(result).to.equal("1456");
      });
 
      it ('modern to romain', () => {
           let result = package ("1456","romain") ; 
-          assert.equal(result,"MCDLVI");
+          expect(result).to.equal("MCDLVI");
      });
 
      it ('modern to arabic', () => {
           let result = package ("1456","arabic") ; 
-          assert.equal(result,"۱٤٥٦");
+          expect(result).to.equal("۱٤٥٦");
      });
 
      it ('modern to persian', () => {
           let result = package ("1456","persian") ; 
-          assert.equal(result,"۱۴۵۶");
+          expect(result).to.equal("۱۴۵۶");
      });
 
      it ('arabic to arabic', () => {
           let result = package ("۱٤٥٦","arabic") ; 
-          assert.equal(result,"۱٤٥٦");
+          expect(result).to.equal("۱٤٥٦");
      });  
 
      it ('arabic to modern', () => {
           let result = package ("۱٤٥٦","modern") ; 
-          assert.equal(result,"1456");
+          expect(result).to.equal("1456");
      });  
 
      it ('arabic to romain', () => {
           let result = package ("۱٤٥٦","romain") ; 
-          assert.equal(result,"MCDLVI");
+          expect(result).to.equal("MCDLVI");
      });     
  
      it ('arabic to persian', () => {
           let result = package ("۱٤٥٦","persian") ; 
-          assert.equal(result,"۱۴۵۶");
+          expect(result).to.equal("۱۴۵۶");
      });  
 
      it ('persian to persian', () => {
           let result = package ("۱۴۵۶","persian") ; 
-          assert.equal(result,"۱۴۵۶");
+          expect(result).to.equal("۱۴۵۶");
      });
 
      it ('persian to arabic', () => {
           let result = package ("۱۴۵۶","arabic") ; 
-          assert.equal(result,"۱٤٥٦");
+          expect(result).to.equal("۱٤٥٦");
      }); 
 
      it ('persian to romain', () => {
           let result = package ("۱۴۵۶","romain") ; 
-          assert.equal(result,"MCDLVI");
+          expect(result).to.equal("MCDLVI");
      });
 
 
      it ('persian to modern', () => {
           let result = package ("۱۴۵۶","modern") ; 
-          assert.equal(result,"1456");
+          expect(result).to.equal("1456");
      });
 
      it ('romain Error', () => {
           let result = package ("45000","romain") ; 
-          assert.equal(result,"Error .. invalid romain number < the numbers should be in [  1 .. 3999 ] >");
+          expect(result).to.equal("Error .. invalid romain number < the numbers should be in [  1 .. 3999 ] >");
      });
 
      it ('Input Error', () => {
           let result = package ("45IMED00","arabic") ; 
-          assert.equal(result,"Error .. invalid number !!");
+          expect(result).to.equal("Error .. invalid number !!");
      });
 }); 
      
