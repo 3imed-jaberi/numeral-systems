4055f782336cda6f7a289f567a3996c26dc38d1a
3imed-jaberi <imed_jebari@hotmail.fr>
2020-01-14 23:55:47 +0100

    make the 3 ways more clean 🌵 ..

--- src/validation/index.js
+++ src/validation/index.js
@@ -6,16 +6,18 @@ const validationInputNumerals = (number) => {
 
   number = `${number}`;
 
-  // get all numerals in one table .. 
+// ======================== get all numerals in one table .. ======================== //
   // 1st methode but not support in NODE JS V 10.15.0 ... 
-    // let allNumeralsSystem = getBaseNumerals('ALL').flat();
-  // 2nd methode  ...
-    // let allNumeralsSystem = getBaseNumerals('ALL').reduce((acc, val) => acc.concat(val), []);
+    // let allNumeralsSystem = getBaseNumerals('all').flat();
+  // 2nd methode <complex> ...
+    // let allNumeralsSystem = getBaseNumerals('all').reduce((acc, val) => acc.concat(val), []);
   // 3rd methode ...
-  let allNumeralsSystem = [].concat(...getBaseNumerals('ALL'));
+    // let allNumeralsSystem = [].concat(...getBaseNumerals('all'));
+// ================================================================================== //
+
 
   for ( let i = 0; i < number.length; i++ ) {
-    let searchInNumerals = allNumeralsSystem.filter(element => element === number.charAt(i)) ;                    
+    let searchInNumerals = ([].concat(...getBaseNumerals('all'))).filter(element => element === number.charAt(i)) ;                    
     if (searchInNumerals.length === 0) {
       return -1;
     }
