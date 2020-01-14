0f1c88f117e6fae9fd2e3071848e3440dbfbedbe
3imed-jaberi <imed_jebari@hotmail.fr>
2020-01-15 00:30:47 +0100

    add constants dir 📁 ..

---
+++ src/constants/index.js
@@ -0,0 +1,19 @@
+
+const __ARABIC__ = 'arabic';
+
+const __MODERN__ = 'modern';
+
+const __PERSIAN__ = 'persian'
+
+const __ROMAIN__ = 'romain';
+
+const __ALL__ = 'all';
+
+module.exports = {
+  __ARABIC__,
+  __ALL__,
+  __MODERN__,
+  __PERSIAN__,
+  __ROMAIN__,
+  
+}
