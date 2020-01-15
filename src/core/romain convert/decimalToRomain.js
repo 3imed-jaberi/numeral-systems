449a0a8b4bd61b39353c0cdd4fd16c0a46b39968
3imed-jaberi <imed_jebari@hotmail.fr>
2020-01-15 01:43:08 +0100

    remove comment ❌ ..

--- src/core/romain convert/decimalToRomain.js
+++ src/core/romain convert/decimalToRomain.js
@@ -5,16 +5,16 @@ const { Decimal } = require('./romain.json');
 const DecimalToRomain = (number) => {
 
   let result = [],
-  decimal = Decimal.decimal, //base values ..
-  symbol = Decimal.symbol, //roman symbols ..
+  decimal = Decimal.decimal,
+  symbol = Decimal.symbol,
   i = 0;
       
-  while(number){  //repeat process until num is not 0 ..
-    while(parseInt(number/decimal[i])){  //first base value that divides num is largest base value ..
-      result.push(symbol[i]);  //print roman symbol equivalent to largest base value ..
-      number -= decimal[i];  //subtract largest base value from num ..
+  while(number){
+    while(parseInt(number/decimal[i])){
+      result.push(symbol[i]);
+      number -= decimal[i];
     }
-    i++;  //move to next base value to divide num ..
+    i++;
   }
 
   return result.join('');
--- src/core/romain convert/romainToDecimal.js
+++ src/core/romain convert/romainToDecimal.js
@@ -4,28 +4,19 @@ const { Romain } = require('./romain.json');
 
 const RomainToDecimal = (number) => {
 
-  let decimal = Romain.decimal, //base values
-  symbol = Romain.symbol, //roman symbols
-  // Initialize result
-  res = 0;
+  let decimal = Romain.decimal, symbol = Romain.symbol, res = 0;
 
-  // Traverse given input
   for (let i = 0; i < number.length; i++){
 
-    // Getting value of symbol s[i]
-    let s1 = decimal[symbol.findIndex(element => element === number[i])] ;
-
+    let s1 = decimal[symbol.findIndex(element => element === number[i])];
+    
     if (i+1 < number.length){
-      // Getting value of symbol s[i+1]
-      let s2 = decimal[symbol.findIndex(element => element === number[i+1])] ;; 
-      // Comparing both values
+      let s2 = decimal[symbol.findIndex(element => element === number[i+1])]; 
       if (s1 >= s2){
-        // Value of current symbol is greater
-        // or equal to the next symbol
         res = res + s1;
       }else{
         res = res + s2 - s1;
-        i++; // Value of current symbol is
+        i++;
       }
     }else{
       res = res + s1;
