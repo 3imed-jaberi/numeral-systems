8748024d7427ce3031299d1b537459caa6b5019c
Oleg Kovalov <oleg@hey.com>
2021-03-02 11:51:37 +0100

    more alphabets

--- src/core/index.js
+++ src/core/index.js
@@ -1,48 +1,57 @@
-const { DecimalToRomain, RomainToDecimal } = require ('./romain convert');
-const { getBaseNumerals, detecteNumeralTypeOfInput } = require ('../utils');
-const { __ARABIC__, __MODERN__, __PERSIAN__,  __ROMAIN__, __ERROR_ROMAIN__ } = require('../constants');
+const { DecimalToRomain, RomainToDecimal } = require('./romain convert');
+const { getBaseNumerals, detecteNumeralTypeOfInput } = require('../utils');
+const { __ARABIC__, __KHMER__, __LAO__, __MODERN__, __PERSIAN__, __ROMAIN__, __THAI__, __ERROR_ROMAIN__ } = require('../constants');
 
 
 
 const convert = (number, typeResultNumerals) => {
 
-  number = `${number}`; 
+  number = `${number}`;
 
 
-  if ((typeResultNumerals === __ROMAIN__) && (detecteNumeralTypeOfInput(number) !== __ROMAIN__)){
-    return ((+number < 1) || (+number > 3999) 
-                            ? 
-       new Error(__ERROR_ROMAIN__).message
-                            :
-       DecimalToRomain(convert(number, __MODERN__)));
-  }else if (detecteNumeralTypeOfInput(number) === __ROMAIN__) {
+  if ((typeResultNumerals === __ROMAIN__) && (detecteNumeralTypeOfInput(number) !== __ROMAIN__)) {
+    return ((+number < 1) || (+number > 3999)
+      ?
+      new Error(__ERROR_ROMAIN__).message
+      :
+      DecimalToRomain(convert(number, __MODERN__)));
+  } else if (detecteNumeralTypeOfInput(number) === __ROMAIN__) {
     switch (typeResultNumerals) {
-      case __MODERN__: {
-        return RomainToDecimal(number);
-      }
       case __ARABIC__: {
         return convert(RomainToDecimal(number), __ARABIC__);
       }
+      case __KHMER__: {
+        return convert(RomainToDecimal(number), __KHMER__);
+      }
+      case __LAO__: {
+        return convert(RomainToDecimal(number), __LAO__);
+      }
+      case __MODERN__: {
+        return convert(RomainToDecimal(number), __MODERN__);
+      }
       case __PERSIAN__: {
-       return convert(RomainToDecimal(number), __PERSIAN__);
+        return convert(RomainToDecimal(number), __PERSIAN__);
+      }
+      case __THAI__: {
+        return convert(RomainToDecimal(number), __THAI__);
       }
       default: {
-       return number;
+        return number;
       }
-    }       
-  }else {             
+    }
+  } else {
     // get array basic numeral system ..
-    let typeResultConvert = getBaseNumerals(typeResultNumerals) ;
+    let typeResultConvert = getBaseNumerals(typeResultNumerals);
     // set the dedected input type in array .. 
-    let typeInputNumber = getBaseNumerals(detecteNumeralTypeOfInput(number)) ;   
+    let typeInputNumber = getBaseNumerals(detecteNumeralTypeOfInput(number));
     // array of the number .. 
     let inputNumber = number.split('');
     // array of each chiffre in the input number .. 
     let IndexTAB = [];
     // put the same number in the new array .. 
-    let arrayConvertNumber = [] ;
+    let arrayConvertNumber = [];
     // get the index of each chiffre in the input number .. 
-    inputNumber.forEach( element => {
+    inputNumber.forEach(element => {
       typeInputNumber.forEach((value, index) => (element === value) ? IndexTAB.push(index) : null);
     });
     //
--- test/package-test.js
+++ test/package-test.js
@@ -1,12 +1,15 @@
 const { expect } = require('chai');
-const package = require('../index'); 
-const { 
-  __ARABIC__, 
-  __MODERN__, 
-  __PERSIAN__, 
-  __ROMAIN__,
-  __ERROR_INVALID__, 
-  __ERROR_ROMAIN__ 
+const package = require('../index');
+const {
+     __ARABIC__,
+     __KHMER__,
+     __LAO__,
+     __MODERN__,
+     __PERSIAN__,
+     __ROMAIN__,
+     __THAI__,
+     __ERROR_INVALID__,
+     __ERROR_ROMAIN__
 } = require('../src/constants');
 
 
@@ -15,94 +18,266 @@ describe(
 `\n*********************************\n*  WELCOME TO THE Pakcage TEST  *\n*********************************`,
  () => {
 
-     it ('romain to romain', () => {
-          let result = package('MCDLVI', __ROMAIN__); 
+     // arabic
+     it('arabic to arabic', () => {
+          let result = package('۱٤٥٦', __ARABIC__);
+          expect(result).to.equal('۱٤٥٦');
+     });
+     
+     it('arabic to khmer', () => {
+          let result = package('۱٤٥٦', __KHMER__);
+          expect(result).to.equal('១៤៥៦');
+     });
+     
+     it('arabic to lao', () => {
+          let result = package('۱٤٥٦', __LAO__);
+          expect(result).to.equal('໑໔໕໖');
+     });
+     
+     it('arabic to modern', () => {
+          let result = package('۱٤٥٦', __MODERN__);
+          expect(result).to.equal('1456');
+     });
+     
+     it('arabic to persian', () => {
+          let result = package('۱٤٥٦', __PERSIAN__);
+          expect(result).to.equal('۱۴۵۶');
+     });
+     
+     it('arabic to romain', () => {
+          let result = package('۱٤٥٦', __ROMAIN__);
+          expect(result).to.equal('MCDLVI');
+     });
+     
+     it('arabic to thai', () => {
+          let result = package('۱٤٥٦', __THAI__);
+          expect(result).to.equal('๑๔๕๖');
+     });
+     
+     // khmer
+     it('khmer to arabic', () => {
+          let result = package('១៤៥៦', __ARABIC__);
+          expect(result).to.equal('۱٤٥٦');
+     });
+     
+     it('khmer to khmer', () => {
+          let result = package('១៤៥៦', __KHMER__);
+          expect(result).to.equal('១៤៥៦');
+     });
+     
+     it('khmer to lao', () => {
+          let result = package('១៤៥៦', __LAO__);
+          expect(result).to.equal('໑໔໕໖');
+     });
+     
+     it('khmer to modern', () => {
+          let result = package('១៤៥៦', __MODERN__);
+          expect(result).to.equal('1456');
+     });
+     
+     it('khmer to persian', () => {
+          let result = package('១៤៥៦', __PERSIAN__);
+          expect(result).to.equal('۱۴۵۶');
+     });
+     
+     it('khmer to romain', () => {
+          let result = package('១៤៥៦', __ROMAIN__);
           expect(result).to.equal('MCDLVI');
      });
 
-     it ('romain to modern', () => {
-          let result = package('MCDLVI', __MODERN__); 
-          expect(result.toString()).to.equal('1456');
+     it('khmer to thai', () => {
+          let result = package('១៤៥៦', __THAI__);
+          expect(result).to.equal('๑๔๕๖');
      });
 
-     it ('romain to arabic', () => {
-          let result = package('MCDLVI', __ARABIC__); 
+     // lao
+     it('lao to arabic', () => {
+          let result = package('໑໔໕໖', __ARABIC__);
           expect(result).to.equal('۱٤٥٦');
-     }); 
-
-     it ('romain to persian', () => {
-          let result = package('MCDLVI', __PERSIAN__); 
+     });
+     
+     it('lao to khmer', () => {
+          let result = package('໑໔໕໖', __KHMER__);
+          expect(result).to.equal('១៤៥៦');
+     });
+     
+     it('lao to lao', () => {
+          let result = package('໑໔໕໖', __LAO__);
+          expect(result).to.equal('໑໔໕໖');
+     });
+     
+     it('lao to modern', () => {
+          let result = package('໑໔໕໖', __MODERN__);
+          expect(result).to.equal('1456');
+     });
+     
+     it('lao to persian', () => {
+          let result = package('໑໔໕໖', __PERSIAN__);
           expect(result).to.equal('۱۴۵۶');
+     });
+     
+     it('lao to romain', () => {
+          let result = package('໑໔໕໖', __ROMAIN__);
+          expect(result).to.equal('MCDLVI');
+     });
+
+     it('lao to thai', () => {
+          let result = package('໑໔໕໖', __THAI__);
+          expect(result).to.equal('๑๔๕๖');
+     });
+     
+     // modern
+     it('modern to arabic', () => {
+          let result = package('1456', __ARABIC__);
+          expect(result).to.equal('۱٤٥٦');
      });     
 
-     it ('modern to modern', () => {
-          let result = package('1456', __MODERN__); 
-          expect(result).to.equal('1456');
-     });
-
-     it ('modern to romain', () => {
-          let result = package('1456', __ROMAIN__); 
-          expect(result).to.equal('MCDLVI');
-     });
-
-     it ('modern to arabic', () => {
-          let result = package('1456', __ARABIC__); 
-          expect(result).to.equal('۱٤٥٦');
-     });
-
-     it ('modern to persian', () => {
-          let result = package('1456', __PERSIAN__); 
-          expect(result).to.equal('۱۴۵۶');
-     });
-
-     it ('arabic to arabic', () => {
-          let result = package('۱٤٥٦', __ARABIC__); 
-          expect(result).to.equal('۱٤٥٦');
-     });  
-
-     it ('arabic to modern', () => {
-          let result = package('۱٤٥٦', __MODERN__); 
-          expect(result).to.equal('1456');
-     });  
-
-     it ('arabic to romain', () => {
-          let result = package('۱٤٥٦', __ROMAIN__); 
-          expect(result).to.equal('MCDLVI');
+     it('modern to khmer', () => {
+          let result = package('1456', __KHMER__);
+          expect(result).to.equal('១៤៥៦');
      });     
- 
-     it ('arabic to persian', () => {
-          let result = package('۱٤٥٦', __PERSIAN__); 
-          expect(result).to.equal('۱۴۵۶');
-     });  
 
-     it ('persian to persian', () => {
-          let result = package('۱۴۵۶', __PERSIAN__); 
-          expect(result).to.equal('۱۴۵۶');
-     });
-
-     it ('persian to arabic', () => {
-          let result = package('۱۴۵۶', __ARABIC__); 
-          expect(result).to.equal('۱٤٥٦');
-     }); 
-
-     it ('persian to romain', () => {
-          let result = package('۱۴۵۶', __ROMAIN__); 
-          expect(result).to.equal('MCDLVI');
-     });
-
-
-     it ('persian to modern', () => {
-          let result = package('۱۴۵۶', __MODERN__); 
+     it('modern to lao', () => {
+          let result = package('1456', __LAO__);
+          expect(result).to.equal('໑໔໕໖');
+     });     
+     
+     it('modern to modern', () => {
+          let result = package('1456', __MODERN__);
           expect(result).to.equal('1456');
      });
+     
+     it('modern to persian', () => {
+          let result = package('1456', __PERSIAN__);
+          expect(result).to.equal('۱۴۵۶');
+     });
+     
+     it('modern to romain', () => {
+          let result = package('1456', __ROMAIN__);
+          expect(result).to.equal('MCDLVI');
+     });
+     
+     it('modern to thai', () => {
+          let result = package('1456', __THAI__);
+          expect(result).to.equal('๑๔๕๖');
+     });
+     
+     // persian     
+     it('persian to arabic', () => {
+          let result = package('۱۴۵۶', __ARABIC__);
+          expect(result).to.equal('۱٤٥٦');
+     });     
 
-     it ('romain Error', () => {
-          let result = package('45000', __ROMAIN__); 
+     it('persian to khmer', () => {
+          let result = package('۱۴۵۶', __KHMER__);
+          expect(result).to.equal('១៤៥៦');
+     });     
+
+     it('persian to lao', () => {
+          let result = package('۱۴۵۶', __LAO__);
+          expect(result).to.equal('໑໔໕໖');
+     });     
+     
+     it('persian to modern', () => {
+          let result = package('۱۴۵۶', __MODERN__);
+          expect(result).to.equal('1456');
+     });
+     
+     it('persian to persian', () => {
+          let result = package('۱۴۵۶', __PERSIAN__);
+          expect(result).to.equal('۱۴۵۶');
+     });
+     
+     it('persian to romain', () => {
+          let result = package('۱۴۵۶', __ROMAIN__);
+          expect(result).to.equal('MCDLVI');
+     });
+     
+     it('persian to thai', () => {
+          let result = package('۱۴۵۶', __THAI__);
+          expect(result).to.equal('๑๔๕๖');
+     });
+
+     // romain
+     it('romain to arabic', () => {
+          let result = package('MCDLVI', __ARABIC__);
+          expect(result).to.equal('۱٤٥٦');
+     });     
+
+     it('romain to khmer', () => {
+          let result = package('MCDLVI', __KHMER__);
+          expect(result).to.equal('១៤៥៦');
+     });     
+
+     it('romain to lao', () => {
+          let result = package('MCDLVI', __LAO__);
+          expect(result).to.equal('໑໔໕໖');
+     });     
+     
+     it('romain to modern', () => {
+          let result = package('MCDLVI', __MODERN__);
+          expect(result).to.equal('1456');
+     });
+     
+     it('romain to persian', () => {
+          let result = package('MCDLVI', __PERSIAN__);
+          expect(result).to.equal('۱۴۵۶');
+     });
+     
+     it('romain to romain', () => {
+          let result = package('MCDLVI', __ROMAIN__);
+          expect(result).to.equal('MCDLVI');
+     });
+     
+     it('romain to thai', () => {
+          let result = package('MCDLVI', __THAI__);
+          expect(result).to.equal('๑๔๕๖');
+     });
+
+     // thai
+     it('thai to arabic', () => {
+          let result = package('๑๖๘๓', __ARABIC__);
+          expect(result).to.equal('۱٦۸۳');
+     });
+     
+     it('thai to khmer', () => {
+          let result = package('๑๖๘๓', __KHMER__);
+          expect(result).to.equal('១៦៨៣');
+     });
+     
+     it('thai to lao', () => {
+          let result = package('๑๖๘๓', __LAO__);
+          expect(result).to.equal('໑໖໘໓');
+     });
+     
+     it('thai to modern', () => {
+          let result = package('๑๖๘๓', __MODERN__);
+          expect(result).to.equal('1683');
+     });
+     
+     it('thai to persian', () => {
+          let result = package('๑๖๘๓', __PERSIAN__);
+          expect(result).to.equal('۱۶۸۳');
+     });
+     
+     it('thai to romain', () => {
+          let result = package('๑๖๘๓', __ROMAIN__);
+          expect(result).to.equal('MDCLXXXIII');
+     });
+
+     it('thai to thai', () => {
+          let result = package('๑๖๘๓', __THAI__);
+          expect(result).to.equal('๑๖๘๓');
+     });
+
+     // error
+     it('romain Error', () => {
+          let result = package('45000', __ROMAIN__);
           expect(result).to.equal(__ERROR_ROMAIN__);
      });
 
-     it ('input error', () => {
-          let result = package('45IMED00', __ARABIC__); 
+     it('input error', () => {
+          let result = package('45IMED00', __ARABIC__);
           expect(result).to.equal(__ERROR_INVALID__);
      });
 });
