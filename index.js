b225e0a05a36ad9a518935f4a20f47d8aae72ddb
3imed-jaberi <imed_jebari@hotmail.fr>
2021-10-03 16:08:47 +0200

    improve perf. --rocket 🚀 ..

--- index.js
+++ index.js
@@ -11,45 +11,31 @@
 /**
  * Modules as JSON files.
  */
-const {
-  all,
-  arabic,
-  khmer,
-  lao,
-  modern,
-  persian,
-  romain,
-  thai,
-  romain_payload: {
-    Decimal,
-    Romain
-  }
-} = require('./payload.json')
-const {
-  __ALL__,
-  __ARABIC__,
-  __KHMER__,
-  __LAO__,
-  __MODERN__,
-  __PERSIAN__,
-  __ROMAIN__,
-  __THAI__
-} = require('./consts.json')
+const BASE_NUMERALS = require('./base_numerals.json')
 
 /**
- * check if the number have x type.
+ * const global types
+ */
+const __ALL__ = 'all'
+const __ARABIC__ = 'arabic'
+const __KHMER__ = 'khmer'
+const __LAO__ = 'lao'
+const __MODERN__ = 'modern'
+const __PERSIAN__ = 'persian'
+const __ROMAIN__ = 'romain'
+const __THAI__ = 'thai'
+
+/**
+ * Same as Array.prototype.flat()
  *
+ * @link https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/flat
  * @api private
  */
-function haveType (number, type) {
-  const detectedResult = [];
-  (number.toString().split('')).forEach(element => {
-    const res = getBaseNumerals(type).filter(value => value === element)
-    if (res.length !== 0) {
-      detectedResult.push(res)
-    }
-  })
-  return detectedResult
+function flat (array) {
+  // we don't need valdiation here because we use it internel.
+  return process.version.startsWith('v10')
+    ? [].concat(...array)
+    : array.flat()
 }
 
 /**
@@ -58,16 +44,7 @@ function haveType (number, type) {
  * @api private
  */
 function getBaseNumerals (typeResultNumber) {
-  switch (typeResultNumber) {
-    case __ARABIC__: { return arabic }
-    case __KHMER__: { return khmer }
-    case __LAO__: { return lao }
-    case __MODERN__: { return modern }
-    case __PERSIAN__: { return persian }
-    case __ROMAIN__: { return romain }
-    case __THAI__: { return thai }
-    default: { return all }
-  }
+  return BASE_NUMERALS[typeResultNumber]
 }
 
 /**
@@ -76,31 +53,48 @@ function getBaseNumerals (typeResultNumber) {
  * @api private
  */
 function getType (number) {
-  if (haveType(number, __KHMER__).length !== 0) {
-    return __KHMER__
+  const inputNumberList = number.toString().split('')
+  // const specialPersianNumbersOverArabic = ['۴', '۵', '۶']
+
+  const keysList = [
+    // __ARABIC__,
+    __KHMER__,
+    __LAO__,
+    __MODERN__,
+    // __PERSIAN__,
+    __ROMAIN__,
+    __THAI__
+  ]
+
+  for (const itemNumber of inputNumberList) {
+    for (const itemKey of keysList) {
+      if (getBaseNumerals(itemKey).includes(itemNumber)) return itemKey
+    }
   }
 
-  if (haveType(number, __LAO__).length !== 0) {
-    return __LAO__
-  }
+  // NOTE: Keep this to be not breaking change.
+  // TODO: remove this behave into next major bump release.
+  // look to use other options like override PERSIAN over ARABIC and/or inverse.
+  const getCharLengthBy = (type) =>
+    inputNumberList
+      .map((element) => {
+        const result = getBaseNumerals(type).filter(value => value === element)
+        return result.length !== 0 ? result : null
+      })
+      .filter((el) => el !== null)
 
-  if (haveType(number, __MODERN__).length !== 0) {
-    return __MODERN__
-  }
+  const persianLength = getCharLengthBy(__PERSIAN__).length
+  const arabicLength = getCharLengthBy(__ARABIC__).length
+  return (persianLength > arabicLength) ? __PERSIAN__ : __ARABIC__
+}
 
-  if (haveType(number, __PERSIAN__).length > haveType(number, __ARABIC__).length) {
-    return __PERSIAN__
-  }
-
-  if (haveType(number, __ROMAIN__).length !== 0) {
-    return __ROMAIN__
-  }
-
-  if (haveType(number, __THAI__).length !== 0) {
-    return __THAI__
-  }
-
-  return __ARABIC__
+/**
+ * check if the passed type value was romain type.
+ *
+ * @api private
+ */
+function isRomainType (value) {
+  return value === __ROMAIN__
 }
 
 /**
@@ -110,8 +104,12 @@ function getType (number) {
  */
 function decimalToRomain (number) {
   const result = []
-  const decimal = Decimal.decimal
-  const symbol = Decimal.symbol
+  const {
+    romain_decimal: {
+      chiffre: decimal,
+      symbol
+    }
+  } = BASE_NUMERALS
   let i = 0
 
   while (number) {
@@ -131,9 +129,13 @@ function decimalToRomain (number) {
  * @api private
  */
 const romainToDecimal = (number) => {
-  const decimal = Romain.decimal
-  const symbol = Romain.symbol
-  let res = 0
+  const {
+    romain_raw: {
+      chiffre: decimal,
+      symbol
+    }
+  } = BASE_NUMERALS
+  let result = 0
 
   for (let i = 0; i < number.length; i++) {
     const s1 = decimal[symbol.findIndex(element => element === number[i])]
@@ -141,18 +143,18 @@ const romainToDecimal = (number) => {
     if (i + 1 < number.length) {
       const s2 = decimal[symbol.findIndex(element => element === number[i + 1])]
       if (s1 >= s2) {
-        res += s1
+        result += s1
       } else {
-        res += s2 - s1
+        result += s2 - s1
         i++
       }
     } else {
-      res += s1
+      result += s1
       i++
     }
   }
 
-  return res
+  return result
 }
 
 /**
@@ -161,37 +163,23 @@ const romainToDecimal = (number) => {
  * @api private
  */
 function convertHandler (number, typeResultNumerals) {
-  if (typeResultNumerals === __ROMAIN__ && getType(number) !== __ROMAIN__) {
+  if (isRomainType(typeResultNumerals) && !isRomainType(getType(number))) {
     if (+number < 1 || +number > 3999) {
-      throw new Error('Invalid romain number <the numbers should be in [1 .. 3999]>')
+      throw new Error(
+        'invalid romain number: please check your number' +
+        'and make sure that\'s valid because the supported' +
+        'romain number should be in this interval [1 .. 3999] \n' +
+        'you can find more details here:' +
+        'https://github.com/3imed-jaberi/numeral-systems#some-informations'
+      )
     }
     return decimalToRomain(convertHandler(number, __MODERN__))
   }
 
-  if (getType(number) === __ROMAIN__) {
-    switch (typeResultNumerals) {
-      case __ARABIC__: {
-        return convertHandler(romainToDecimal(number), __ARABIC__)
-      }
-      case __KHMER__: {
-        return convertHandler(romainToDecimal(number), __KHMER__)
-      }
-      case __LAO__: {
-        return convertHandler(romainToDecimal(number), __LAO__)
-      }
-      case __MODERN__: {
-        return convertHandler(romainToDecimal(number), __MODERN__)
-      }
-      case __PERSIAN__: {
-        return convertHandler(romainToDecimal(number), __PERSIAN__)
-      }
-      case __THAI__: {
-        return convertHandler(romainToDecimal(number), __THAI__)
-      }
-      default: {
-        return number
-      }
-    }
+  if (isRomainType(getType(number))) {
+    return !isRomainType(typeResultNumerals)
+      ? convertHandler(romainToDecimal(number), typeResultNumerals)
+      : number
   }
 
   // get array basic numeral system.
@@ -200,20 +188,37 @@ function convertHandler (number, typeResultNumerals) {
   const typeInputNumber = getBaseNumerals(getType(number))
   // array of the number.
   const inputNumber = number.toString().split('')
-  // array of each chiffre in the input number.
-  const indexTAB = []
   // get the index of each chiffre in the input number.
-  inputNumber.forEach(element => {
-    typeInputNumber.forEach((value, index) => {
-      if (element === value) {
-        indexTAB.push(index)
-      }
-    })
-  })
+  const indexTAB = inputNumber
+    .map((element) =>
+      typeInputNumber
+        .map((value, index) => {
+          if (element === value) {
+            return index
+          }
+          return null
+        })
+        .filter((el) => el !== null)
+    )
   // put the same number in the new array.
   const arrayConvertNumber = indexTAB.map(element => typeResultConvert[element])
   // translate the new array number to string.
   return arrayConvertNumber.join('')
+}
+
+/**
+ * check if the passed number is supported or not.
+ *
+ * @api private
+ */
+function isSupportedNumber (number) {
+  const allNumeralsSystem = flat(getBaseNumerals(__ALL__))
+  const inputNumberList = number.toString().split('')
+  for (const itemNumber of inputNumberList) {
+    if (!allNumeralsSystem.includes(itemNumber)) return false
+  }
+
+  return true
 }
 
 /**
@@ -222,17 +227,14 @@ function convertHandler (number, typeResultNumerals) {
  * @api public
  */
 function numeralSystems (number, typeResultNumerals) {
-  // 1st method (not supported with Node.js v10): getBaseNumerals(__ALL__).flat()
-  // 2nd method: getBaseNumerals(__ALL__).reduce((acc, val) => acc.concat(val), [])
-  // 3rd method
-  const allNumeralsSystem = [].concat(...getBaseNumerals(__ALL__))
-
-  number.toString().split('').forEach(char => {
-    const searchInNumerals = allNumeralsSystem.filter(element => element === char)
-    if (searchInNumerals.length === 0) {
-      throw new Error('Invalid number !')
-    }
-  })
+  if (!isSupportedNumber(number)) {
+    throw new Error(
+      `input number ${number} is invalid or not supported. \n` +
+      'this module support only ["arabic", "khmer", "lao", "modern"' +
+      ', "persian", "romain", "thai"]. \n you can find more about this types' +
+      'here: https://github.com/3imed-jaberi/numeral-systems#some-informations'
+    )
+  }
 
   return convertHandler(number, typeResultNumerals)
 }
